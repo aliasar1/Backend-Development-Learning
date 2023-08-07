2060347cea41b5777f9ae3e603f743b4f91f23d5
Ali Asar Khowaja <khowajaaliasar@gmail.com>
2023-08-08 00:48:40 +0500

    Added Rentals in API

--- exercises/movies_genre/index.js
+++ exercises/movies_genre/index.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 const genres = require('./routes/genres');
-const movies = require('./routes/movies')
+const movies = require('./routes/movies');
+const customers = require('./routes/customers');
+const rentals = require('./routes/rentals');
 const express = require('express');
-const app = express()
+const app = express();
 
 mongoose.connect('mongodb://127.0.0.1/movies_genre', {
         family: 4,
@@ -13,6 +15,8 @@ mongoose.connect('mongodb://127.0.0.1/movies_genre', {
 app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/api/movies', movies);
+app.use('/api/customers', customers);
+app.use('/api/rentals', rentals);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
