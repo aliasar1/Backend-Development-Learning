96c6eafbbb0ddec1d7cccc8e79d768097c08c0d7
Ali Asar Khowaja <khowajaaliasar@gmail.com>
2023-08-14 17:11:05 +0500

    Testing Strings and Arrays.

--- testing-demo/tests/lib.test.js
+++ testing-demo/tests/lib.test.js
@@ -18,3 +18,33 @@ describe('absolute', ()=>{
     });
 });
 
+describe('greet', ()=>{
+    it('should return a greeting message', ()=>{
+        const result = lib.greet('Ali');
+        expect(result).toMatch(/Ali/);
+        expect(result).toContain('Ali');
+    });
+});
+
+describe('getCurrencies', ()=>{
+    it('should return a supported currencies', ()=>{
+        const result = lib.getCurrencies();
+        // Too general
+        // expect(result).toBeDefined();
+        // expect(result).not.toBeNull();
+
+        // Too specific
+        // expect(result[0]).toBe('USD');
+        // expect(result[1]).toBe('AUD');
+        // expect(result[2]).toBe('EUR');
+        // expect(result.length).toBe(3);
+
+        // Proper way - balanced way
+        // expect(result).toContain('USD');
+        // expect(result).toContain('AUD');
+        // expect(result).toContain('EUR');
+
+        // OR IDEAL WAY
+        expect(result).toEqual(expect.arrayContaining(['USD', 'AUD', 'EUR']));
+    });
+});
